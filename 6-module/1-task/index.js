57b074ab1d343cf1406e89c4e5cccfe05e4f17c9
Ivan Tripolyev <it000309@gmail.com>
2021-03-11 09:57:22 +0300

    6-module

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,29 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = document.createElement('table');
+    this.elem.innerHTML = `
+    <thead>
+      <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+      </tr>
+  </thead>`;
+  `<tbody>
+    <tr>
+      <td>"${rows.name}"</td>
+      <td>"${rows.age}"</td>
+      <td>"${rows.salary}"</td>
+      <td>"${rows.city}"</td>
+      <td><button>X</button></td>
+    </tr>
+</tbody>`
   }
+
+
 }
+
+
